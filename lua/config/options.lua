5b293a325d254bc56aafa69ca6ee151a965fe9fb
InChijun <chijun@cubrid.com>
2025-10-02 07:35:08 +0000

    config nvim setting(no-mouse, side-number, absolute-number

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,3 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.opt.mouse = ""  -- 마우스 클릭 방지
+vim.opt.number = true       -- 모든 줄에 절대 라인번호 표시
+vim.opt.relativenumber = false  -- 상대 라인번호 끄기
